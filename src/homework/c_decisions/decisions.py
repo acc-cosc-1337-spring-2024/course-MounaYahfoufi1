91a92bb303a6aba622192394d9bc244a67a4985c
MounaYahfoufi1 <157323334+MounaYahfoufi1@users.noreply.github.com>
2024-02-10 16:24:31 +0000

    HomeWork2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,17 @@
-#
+def get_options_ratio(option, total):
+    if total == 0:
+        return None
+    return option / total
+
+def get_faculty_rating(rating):
+    if rating >= 0.91:
+        return 'Excellent'
+    elif rating >= 0.85:
+        return 'Very Good'
+    elif rating >= 0.71:
+        return 'Good'
+    elif rating >= 0.66:
+        return 'Needs Improvement'
+    else:
+        return 'Unacceptable'
+
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,9 @@
-#
+import decisions
+
+options = float(input("Enter the options: "))
+total = float(input("Enter the total: "))
+
+
+result = decisions.get_options_ratio(options, total)
+
+print("Faculty Rating:", decisions.get_faculty_rating(result))
