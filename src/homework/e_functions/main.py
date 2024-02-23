1add14c42927857d91a8f0572854a285c3f8ad11
MounaYahfoufi1 <157323334+MounaYahfoufi1@users.noreply.github.com>
2024-02-23 01:01:35 +0000

    Homework 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,13 @@
 #
+from value_return import get_hour, get_minutes, get_seconds
+
+def main():
+    epoch_seconds = 3800
+    hour = get_hour(epoch_seconds)
+    minutes = get_minutes(epoch_seconds)
+    seconds = get_seconds(epoch_seconds)
+
+    print(f"{hour:02d}:{minutes:02d}:{seconds:02d}")
+
+if __name__ == "__main__":
+    main()
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,14 @@
 #
+# value_return.py
+
+def get_hour(epoch_seconds):
+    hours = (epoch_seconds // 3600) % 24
+    return hours
+
+def get_minutes(epoch_seconds):
+    minutes = (epoch_seconds // 60) % 60
+    return minutes
+
+def get_seconds(epoch_seconds):
+    seconds = epoch_seconds % 60
+    return seconds
